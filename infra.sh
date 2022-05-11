aec9712bef338a7f5df66e348d2fa5b6fd9a65c9
Egor Ivanov <egor.ivanov@pm.me>
2022-05-11 22:36:56 +0300

    adding unconditional setup for dcoker-compose

--- infra.sh
+++ infra.sh
@@ -118,7 +118,7 @@ echo "$(date) Installing docker-compose..." |& tee -a ${LOG_FILE_NAME}
 #curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 #chmod +x /usr/local/bin/docker-compose
 
-apt-get install docker-compose
+apt-get install docker-compose -y
 
 
 echo "$(date) Checking docker-compose version" |& tee -a ${LOG_FILE_NAME}
