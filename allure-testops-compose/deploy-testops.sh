0e729ce6e898e818686f18cb217415d4f917ef28
Egor Ivanov <egor.ivanov@pm.me>
2022-07-10 10:53:11 +0300

    override the problems

--- allure-testops-compose/deploy-testops.sh
+++ allure-testops-compose/deploy-testops.sh
@@ -3,8 +3,8 @@
 cd ~
 apt update && apt upgrade -y && apt install docker.io -y
 export DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+mkdir -p $DOCKER_CONFIG/cli-plugins || true
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose || true
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
 git clone https://github.com/qameta/allure-docker-compose.git && \
