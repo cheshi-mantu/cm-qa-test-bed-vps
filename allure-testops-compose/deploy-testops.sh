5b17ffd5a562552ce0acba213c15e14de7dd469f
Egor Ivanov <8607348+cheshi-mantu@users.noreply.github.com>
2022-07-15 21:36:25 +0300

    Update deploy-testops.sh

--- allure-testops-compose/deploy-testops.sh
+++ allure-testops-compose/deploy-testops.sh
@@ -5,7 +5,7 @@ apt update && apt upgrade -y
 apt install docker.io -y
 export DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins || true
-curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose || true
+curl -SL https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose || true
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
 git clone https://github.com/qameta/allure-docker-compose.git && \
